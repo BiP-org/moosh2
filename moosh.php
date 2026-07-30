60806065333df90f8e45289c287e5240d79d5028
Georg Glas <georg@glas.eu.org>
2026-07-30 22:05:27 +0200

    Portiere plugin-* functions from moosh to moosh 2.x #3
    https://github.com/BiP-org/moosh2/issues/3

--- moosh.php
+++ moosh.php
@@ -7,7 +7,23 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require __DIR__ . '/vendor/autoload.php';
+$vendorDir = getenv('COMPOSER_VENDOR_DIR');
+
+if ($vendorDir && file_exists($vendorDir . '/autoload.php')) {
+    $autoloadPath = $vendorDir . '/autoload.php';
+} elseif (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    // 2. Standard local vendor directory
+    $autoloadPath = __DIR__ . '/vendor/autoload.php';
+} elseif (file_exists(__DIR__ . '/../../autoload.php')) {
+    // 3. Installed as a dependency in another project
+    $autoloadPath = __DIR__ . '/../../autoload.php';
+} else {
+    fwrite(STDERR, "Error: Unable to find Composer's autoload.php file.\n");
+    fwrite(STDERR, "Please run 'composer install' or check your COMPOSER_VENDOR_DIR.\n");
+    exit(1);
+}
+
+require $autoloadPath;
 
 use Moosh2\Application;
 
