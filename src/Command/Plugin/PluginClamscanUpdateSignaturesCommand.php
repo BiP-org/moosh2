18e6232dd5373fa4ded1679ff4b4c3eb6f58a7e8
GGeorggg <georg-github.com@glas.eu.org>
2026-09-16 17:05:40 +0200

    command to update the malware signatures
    https://github.com/BiP-org/moosh2/issues/34

---
+++ src/Command/Plugin/PluginClamscanUpdateSignaturesCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Moosh2\Command\Plugin;
+
+use Moosh2\Service\ClamavSignatureManager;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class PluginClamscanUpdateSignaturesCommand extends Command
+{
+    protected function configure(): void
+    {
+        $this
+            ->setName('plugin:clamscan:update-signatures')
+            ->setDescription('Download or update additional ClamAV signatures used by plugin:clamscan')
+            ->setHelp(
+                'Downloads signature files from InterServer (sigs.interserver.net) and ' .
+                'phpMussel (github.com/phpMussel/Signatures) into ' .
+                '~/.moosh2/clamav-signatures/. ' .
+                'The plugin:clamscan command will automatically include this directory ' .
+                'when it exists.'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $manager = new ClamavSignatureManager();
+        $output->writeln('Updating signatures in ' . $manager->getSignatureDir());
+
+        $results = $manager->update();
+        $exitCode = Command::SUCCESS;
+
+        foreach ($results as $filename => $status) {
+            if (str_starts_with($status, 'FAILED')) {
+                $output->writeln("  [FAIL] $filename: $status");
+                $exitCode = Command::FAILURE;
+            } else {
+                $output->writeln("  [ OK ] $filename: $status");
+            }
+        }
+
+        if ($exitCode === Command::SUCCESS) {
+            $output->writeln('All signatures updated successfully.');
+        }
+
+        return $exitCode;
+    }
+}
